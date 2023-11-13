b2a2a44c1bf13ce440f3ed5750d45a2061cafa85
torologium <arkulaxyz@gmail.com>
2023-11-13 19:53:32 +0100

    handle glyphs and utf-8 chars

--- labclock.c
+++ labclock.c
@@ -19,14 +19,15 @@ static pixman_color_t
 		.alpha = 0xffff,
 	};
 
-static const struct wl_buffer_listener wl_buffer_listener = {
-	.release = wl_buffer_release,
-};
-
 static void wl_buffer_release(void *data, struct wl_buffer *wl_buffer) {
 	/* sent by the compositor when it's no longer using this buffer */
 	wl_buffer_destroy(wl_buffer);
 }
+
+static const struct wl_buffer_listener wl_buffer_listener = {
+	.release = wl_buffer_release,
+};
+
 
 static struct wl_buffer *draw_frame(char *text) {
 	/* allocate buffer to be attached to the surface */
@@ -39,8 +40,16 @@ static struct wl_buffer *draw_frame(char *text) {
 	if (data == MAP_FAILED) {
 		close(fd);
 		return NULL;
+	}
 
-  /* premultiplied colors */
+	struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, bufsize);
+	struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0,
+			width, height, stride, WL_SHM_FORMAT_ARGB8888);
+	wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
+	wl_shm_pool_destroy(pool);
+	close(fd);
+
+	/* premultiplied colors */
   pixman_color_t textfgcolor = fgcolor;
 
 	/* Pixman image corresponding to main buffer */
@@ -55,5 +64,33 @@ static struct wl_buffer *draw_frame(char *text) {
 			width, height, NULL, width * 4);
 
 	pixman_image_t *fgfill = pixman_image_create_solid_fill(&textfgcolor);
+
+	/* start drawing at center-left (ypos sets the text baseline) */
+	uint32_t xpos = 0, maxxpos = 0;
+	uint32_t ypos = (height + font->ascent - font->descent) / 2;
+
+	uint32_t codepoint, lastcp = 0, state = UTF8_ACCEPT;
+	for (char *p = text; *p; p++) {
+		/* check for inline ^ commands */
+		if (state == UTF8_ACCEPT && *p == '^') p++; 
+
+		/* return nonzero if more bytes are needed */
+		if (utf8decode(&state, &codepoint, *p)) continue;
+
+    /* rasterize a glyph for a single UTF-32 codepoint */
+		const struct fcft_glyph *glyph = fcft_rasterize_char_utf32(font, codepoint,
+				FCFT_SUBPIXEL_DEFAULT);
+		if (!glyph) continue;
+
+		/* adjust x position based on kerning with previous glyph */
+		long x_kern = 0;
+		if (lastcp)
+			fcft_kerning(font, lastcp, codepoint, &x_kern, NULL);
+		xpos += x_kern;
+		lastcp = codepoint;
+
+    /* apply foreground for subpixel rendered text */
+ 		pixman_image_composite32(
+ 			PIXMAN_OP_OVER, fgfill, glyph->pix, foreground, 0, 0, 0, 0,
+ 			xpos + glyph->x, ypos - glyph->y, glyph->width, glyph->height);
   }
-}
