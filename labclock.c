223e5f2acf7c4bf51de9c05847dff04292bf1fae
torologium <arkulaxyz@gmail.com>
2023-11-13 19:51:10 +0100

    init representation logic

--- labclock.c
+++ labclock.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <pixman.h>
+#include "wlr-layer-shell-unstable-v1-protocol.h"
 
 #define LINE_LEN 128
 
@@ -18,7 +19,28 @@ static pixman_color_t
 		.alpha = 0xffff,
 	};
 
-	/* premultiplied colors */
+static const struct wl_buffer_listener wl_buffer_listener = {
+	.release = wl_buffer_release,
+};
+
+static void wl_buffer_release(void *data, struct wl_buffer *wl_buffer) {
+	/* sent by the compositor when it's no longer using this buffer */
+	wl_buffer_destroy(wl_buffer);
+}
+
+static struct wl_buffer *draw_frame(char *text) {
+	/* allocate buffer to be attached to the surface */
+	int fd = allocate_shm_file(bufsize);
+	if (fd == -1)
+		return NULL;
+
+	uint32_t *data = mmap(NULL, bufsize,
+			PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (data == MAP_FAILED) {
+		close(fd);
+		return NULL;
+
+  /* premultiplied colors */
   pixman_color_t textfgcolor = fgcolor;
 
 	/* Pixman image corresponding to main buffer */
@@ -33,3 +55,5 @@ static pixman_color_t
 			width, height, NULL, width * 4);
 
 	pixman_image_t *fgfill = pixman_image_create_solid_fill(&textfgcolor);
+  }
+}
